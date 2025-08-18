61ca1eca1bcf20849ca3a4b88831711b6c5492a8
MrAbelDev <abelmruso@gmail.com>
2025-08-18 12:46:01 +0200

    ajout middleware auth

--- backend/routes/book.routes.js
+++ backend/routes/book.routes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const bookCtrl = require("../controllers/book.controller");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post("/", bookCtrl.uploadBook);
+router.post("/", auth, bookCtrl.uploadBook);
 router.post("/:id/rating", bookCtrl.rateBook);
 
 router.get("/bestrating", bookCtrl.getBestRated); // más específico
 router.get("/", bookCtrl.getAllBooks); // listado completo
 router.get("/:id", bookCtrl.getOneBook); // detalle por id
 
-router.put("/:id", bookCtrl.modifyBook);
-router.delete("/:id", bookCtrl.deleteBook);
+router.put("/:id", auth, bookCtrl.modifyBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
 
 module.exports = router;
