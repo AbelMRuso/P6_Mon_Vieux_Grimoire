70f54aa65d9ded0baeec78ad6881e7115d3d31c6
MrAbelDev <abelmruso@gmail.com>
2025-08-26 10:29:20 +0200

    configuration CORS mise en /middleware/cors.js, et adaptation du fichier app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("./middleware/cors");
 const path = require("path");
 const app = express();
 const bookRoutes = require("./routes/book.routes");
@@ -9,13 +10,7 @@ const userRoutes = require("./routes/user.routes");
 
 connectDB();
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-    next();
-});
-
+app.use(cors);
 app.use(express.json());
 
 app.use("/api/books", bookRoutes);
